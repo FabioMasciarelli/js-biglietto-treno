df93ea3289e3a18502281cc92dfbdad8b6d2fd9d
Fabio Masciarelli <fabiomasciarelliwd@gmail.com>
2024-03-25 16:46:00 +0100

    exsercise finished

--- js/script.js
+++ js/script.js
@@ -17,22 +17,26 @@ console.log(userKm, typeof userKm);
 const variableKm = 0.21; // number
 const youngDiscount = 0.2; // number
 const oldDiscount = 0.4; // number
+let finalPrice ="";
 
-let finalPrice ="" ;
-
+// calcolo del prezzo del biglietto in base all'età e al numero di Km che vuole fare il cliente
 if (userAge < 18 && userAge > 0) {
-    let finalPrice = (variableKm * userKm) - 0.2; 
+    finalPrice = (variableKm * userKm) - 0.2; 
 } else if (userAge > 65) {
-    let finalPrice = (variableKm * userKm) - 0.4;
+    finalPrice = (variableKm * userKm) - 0.4; 
 } else {
-    let finalPrice = (variableKm * userKm)
+    finalPrice = (variableKm * userKm);
 }
-
 console.log(finalPrice, typeof finalPrice);
+
+//trasformare il numero con soli due valori dopo la virgola
+const number = finalPrice.toFixed(2);
+
+finalMessage = `Abbiamo calcolato il prezzo del tuo biglietto ed è di: ${number} €`; // string
 
 
 // OUTPUT 
-document .getElementById("result") .innerHTML = finalPrice
+document .getElementById("result") .innerHTML = finalMessage;
 
 
 
